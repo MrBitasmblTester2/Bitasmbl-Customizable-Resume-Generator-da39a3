c5bf48edf618bd1c8587d0b429ebce2bbbacd3c7
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-14 21:29:45 +0400

    Add ResumeEditor.tsx

---
+++ src/components/editor/ResumeEditor.tsx
@@ -0,0 +1,1 @@
+import React from 'react';export const ResumeEditor:React.FC=()=>{return(<div className="space-y-4"><h2 className="text-lg font-semibold">Resume Editor</h2><div>Editor controls go here</div></div>);};
